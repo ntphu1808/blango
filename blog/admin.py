2790841b4d4456aecb12242e9677582551892412
Trần Nguyễn <ntphu081995@gmail.com>
2022-01-05 13:02:22 +0000

    Finishing Introduction and Django Admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from blog.models import Tag, Post
 # Register your models here.
+# This PostAdmin class used for define the slug field will be automatically updated
+# when the title field changes.
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields= {"slug": ("title",)}
+
+admin.site.register(Tag)
+admin.site.register(Post, PostAdmin)
