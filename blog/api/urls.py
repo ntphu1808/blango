1cf8b438916e97587e081efb0739fd4c05aa050d
Trần Nguyễn <ntphu081995@gmail.com>
2022-02-15 09:40:23 +0000

    Finish related fields

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -2,11 +2,13 @@ from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.authtoken import views
 
-from blog.api.views import PostList, PostDetail
+
+from blog.api.views import *
 
 urlpatterns = [
     path("posts/", PostList.as_view(), name="api_post_list"),
     path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
+    path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"), #user detail path with the argument by email
     path("auth/", include("rest_framework.urls")),   #add the authentication link.
     path("token-auth/", views.obtain_auth_token), #used for generating a token for clients
 ]
