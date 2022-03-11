2f53228c35a5a9569265ee577c5d5f505829dff0
ntphu1808 <92174439+ntphu1808@users.noreply.github.com>
2022-03-12 02:04:48 +0700

    Update forms.py

--- blango_auth/forms.py
+++ blango_auth/forms.py
@@ -1,15 +1,20 @@
-from crispy_forms.helper import FormHelper
+from crispy_forms.helper import FormHelper #We use FormHelper to set options for a Crispy template tag
 from crispy_forms.layout import Submit
-from django_registration.forms import RegistrationForm
+from django_registration.forms import RegistrationForm 
 
 from blango_auth.models import User
 
 
-class BlangoRegistrationForm(RegistrationForm):
+class BlangoRegistrationForm(RegistrationForm):      #this class provides a custom form for creating a new user account, we then use this form in templates html
     class Meta(RegistrationForm.Meta):
         model = User
 
     def __init__(self, *args, **kwargs):
-        super(BlangoRegistrationForm, self).__init__(*args, **kwargs)
+        # inherite all the attributes of the parent class (RegistrationForm)
+        super(BlangoRegistrationForm, self).__init__(*args, **kwargs) 
+        
+        #and throw in the helper attribute = FormHelper
         self.helper = FormHelper()
+        
+        #Add the submit button with value=Register
         self.helper.add_input(Submit("submit", "Register"))
